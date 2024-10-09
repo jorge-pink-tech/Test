c9748e888e85bc80b712a484971be85824af48b3
Jorge Orjuela <jorge@pink-tech.io>
2024-10-08 22:33:38 -0500

    another update

---
+++ Library/APIs/AuthenticationApi/Tests/Parameters/ConfirmForgotPassword/ConfirmForgotPasswordParameters.swift
@@ -0,0 +1,56 @@
+//
+// Copyright © 2024 PinkTech. All rights reserved.
+//
+
+import Vapor
+
+/// A struct representing the parameters when creating a new password.
+public struct ConfirmForgotPasswordParameters: Decodable {
+    /// The confirmation code of the user.
+    public let confirmationCode: String
+
+    /// The confirmation password of the user.
+    public let confirmPassword: String
+
+    /// The email of the user.
+    public let email: String
+
+    /// The password of the user.
+    public let newPassword: String
+}
+
+extension ConfirmForgotPasswordParameters: Validatable {
+    
+    // MARK: Validatable
+    
+    /// Capable of being validated.
+    public static func validations(_ validations: inout Validations) {
+        validations.add(
+            "confirmationCode",
+            as: String.self,
+            is: !.empty,
+            customFailureDescription: "El código de confirmación no puede estar vacío."
+        )
+        
+        validations.add(
+            "email",
+            as: String.self,
+            is: .email,
+            customFailureDescription: "Email invalido."
+        )
+        
+        validations.add(
+            "newPassword",
+            as: String.self,
+            is: .isValidPassword,
+            customFailureDescription: "La contraseña no cumple con los criterios requeridos."
+        )
+
+        validations.add(
+            "confirmPassword",
+            as: String.self,
+            is: .isValidPassword,
+            customFailureDescription: "La contraseña no cumple con los criterios requeridos."
+        )
+    }
+}
